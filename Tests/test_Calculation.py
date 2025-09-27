2c86b09b69123140f9ed2617d532dad52594ec0d
overav4-tech <overav4@unemi.edu.ec>
2025-09-27 10:39:43 -0500

    Mi primer envio

---
+++ Tests/test_Calculation.py
@@ -0,0 +1,12 @@
+import unittest
+from src.calculator import *
+
+class TestCalculator(unittest.TestCase):
+    def test_suma(self):
+        self.assertEqual(suma(2, 3), 5)
+
+    def test_resta(self):
+        self.assertEqual(restar(5, 3), 2)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ src/calculator.py
@@ -0,0 +1,6 @@
+def suma(a,b):
+    return a + b
+def restar(a,b):
+    return a - b
+
+print(restar(5,3))
