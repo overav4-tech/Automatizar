29e23bf2e599ca7c5b44966036b949f0d4a4a998
overav4-tech <overav4@unemi.edu.ec>
2025-09-27 10:55:23 -0500

    Segundo Envio

--- Tests/test_Calculation.py
+++ Tests/test_Calculation.py
@@ -3,7 +3,7 @@ from src.calculator import *
 
 class TestCalculator(unittest.TestCase):
     def test_suma(self):
-        self.assertEqual(suma(2, 3), 5)
+        self.assertEqual(suma(2, 3), 25)
 
     def test_resta(self):
         self.assertEqual(restar(5, 3), 2)
